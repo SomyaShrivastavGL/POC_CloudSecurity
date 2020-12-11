423d3b46d541a1d80d41d24e6a43df346bb6b735
Akshit Bansal <75518353+akshit-cant-code@users.noreply.github.com>
2020-12-11 14:57:59 +0530

    #Added CF EntityFrameworkCore support (#3)

---
+++ Web Api/DBLayer/Repo/Implementation/EmployeeRepo.cs
@@ -0,0 +1,52 @@
+﻿using DBLayer.Repo.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DBLayer.Repo.Implementation
+{
+    public class EmployeeRepo : IEmployeeRepo
+    {
+        private EmployeeDBContext _ctx;
+        public EmployeeRepo(EmployeeDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<int> Add(Employee employee)
+        {
+            _ctx.Employees.Add(employee);
+            return await _ctx.SaveChangesAsync();
+        }
+
+        public async Task<int> Delete(Employee employee)
+        {
+            _ctx.Employees.Remove(employee);
+            return await _ctx.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> Filter(Expression<Func<Employee, bool>> filter)
+        {
+            return await _ctx.Employees.Where(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> GetAll()
+        {
+            return await _ctx.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> GetEmployee(int Id)
+        {
+            return await _ctx.Employees.FindAsync(Id);
+        }
+
+        public async Task<int> Update(Employee employee)
+        {
+            _ctx.Entry(employee).State = EntityState.Modified;
+            return await _ctx.SaveChangesAsync();
+        }
+    }
+}
---
+++ Web Api/DBLayer/Repo/Interfaces/IEmployeeRepo.cs
@@ -0,0 +1,20 @@
+﻿using Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLayer.Repo.Interfaces
+{
+    interface IEmployeeRepo
+    {
+        Task<int> Add(Employee employee);
+        Task<int> Update(Employee employee);
+        Task<int> Delete(Employee employee);
+        Task<IEnumerable<Employee>> GetAll();
+        Task<IEnumerable<Employee>> Filter(Expression<Func<Employee, bool>> filter);
+        Task<Employee> GetEmployee(int Id);
+    }
+}
