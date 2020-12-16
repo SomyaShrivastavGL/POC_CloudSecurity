4d2a3f4164be6e1fb093d0694be1f7ab992d9d1b
Akshit Bansal <75518353+akshit-cant-code@users.noreply.github.com>
2020-12-16 18:47:24 +0530

    #Added Antiforgery Token  support

---
+++ Web Api/Web Api/Controllers/AntiForgeryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AntiForgeryController : ControllerBase
+    {
+        private IAntiforgery _antiForgery;
+        
+        //Inject AntiForgeryService
+        public AntiForgeryController(IAntiforgery antiforgery)
+        {
+            _antiForgery = antiforgery;
+        }
+
+        public IActionResult Get()
+        {
+            var tokenStore = _antiForgery.GetAndStoreTokens(HttpContext);
+            Response.Cookies.Append("XSRF-REQUEST", tokenStore.CookieToken, new CookieOptions() { HttpOnly = false });
+            return Ok();
+        }
+    }
+}
